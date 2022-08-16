e68a0d8b8d89bd59ab2cee17c653dd1f7af969f2
TolaMironcenko <mironcenkotola@gmail.com>
2022-08-16 18:55:27 +0700

    log.txt

--- main.cpp
+++ main.cpp
@@ -133,17 +133,28 @@ void print_all_cars() {
 }
 
 void rent(string carname) {
+    cout << "Введите ваше имя: ";
+    string username;
+    cin >> username;
     cout << termcolor::white << "Введите время аренды в минутах: ";
     int time_of_rent;
+    ofstream fout("log.txt", std::ios::app);
     cin >> time_of_rent;
     cout << termcolor::white << "Вам необходимо заплатить ";
-    cout << termcolor::green << time_of_rent*get_price_by_carname(carname);
+    int price_to_pay = time_of_rent*get_price_by_carname(carname);
+    cout << termcolor::green << price_to_pay;
     cout << termcolor::white << "p\n";
     cout << termcolor::white << "1. Оплатить\n2. Отказ\n";
     int agree;
     cin >> agree;
     switch(agree) {
         case 1: {
+            fout << "car: " << carname << "\n";
+            fout << "pay: " << price_to_pay << "p" << "\n";
+            fout << "username: " << username << "\n";
+            fout << "time of rent: " << time_of_rent << "min" << "\n";
+            fout << "===================================================================\n";
+            fout.close();
             cout << termcolor::green << "Оплачено\n";
             break;
         }
